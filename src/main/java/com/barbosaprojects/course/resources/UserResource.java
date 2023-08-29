a5f6701fb8fc9cc7c9d53b625413cda2bfa212b2
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-08-29 08:57:24 -0300

    chore: Delete pasta Target IDE

--- src/main/java/com/barbosaprojects/course/resources/UserResource.java
+++ src/main/java/com/barbosaprojects/course/resources/UserResource.java
@@ -19,13 +19,13 @@ public class UserResource {
     UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
