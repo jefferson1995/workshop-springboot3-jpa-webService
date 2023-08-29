f3890861c1c991e04eeeceb1c316fd8db7e6bd21
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-08-29 08:49:10 -0300

    feat: Service layer, component registration

--- src/main/java/com/barbosaprojects/course/repositories/UserRepository.java
+++ src/main/java/com/barbosaprojects/course/repositories/UserRepository.java
@@ -3,6 +3,11 @@ package com.barbosaprojects.course.repositories;
 import com.barbosaprojects.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+    Opcional adicionar a anotação
+    @Respository
+    Porque o JpaRepository já está registrado como um componente do spring
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
